b5896ec04f9bce25793adc03936539d203488ffd
mercer <luhaibing12@gmail.com>
2025-08-04 16:10:59 +0800

    1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("kotlin-kapt")
 }
 
 android {
+
     namespace = "com.mercer.demo"
     compileSdk = 35
 
@@ -23,13 +25,20 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
@@ -42,4 +51,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.yyued:SVGAPlayer-Android:2.6.1")
+    implementation("com.airbnb.android:lottie:6.4.0")
+    implementation("com.caverock:androidsvg:1.4")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    kapt("com.github.bumptech.glide:compiler:4.16.0")
+
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    implementation("com.blankj:utilcodex:1.31.0")
+
 }
